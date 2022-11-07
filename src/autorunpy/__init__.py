ab10e0245dd367fa8316844ed7e060c48eb9fa59
imahdimir <imahdimir@gmail.com>
2022-11-07 18:25:58 +0330

    0.0.5

--- src/autorunpy/__init__.py
+++ src/autorunpy/__init__.py
@@ -1,9 +1,3 @@
 """
 
     """
-
-from src.autorunpy.autorunpy import dl_main_bash
-from src.autorunpy.autorunpy import make_venv
-from src.autorunpy.autorunpy import ret_dirn
-from src.autorunpy.autorunpy import ret_module_2_run_name
-from src.autorunpy.autorunpy import rm_venv
