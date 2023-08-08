ff2be2b10df3df211ac954b1e59a50d8b1320a94
imahdimir <imahdimir@gmail.com>
2023-08-08 12:42:28 +0330

    7.0.0

--- src/autorunpy/util.py
+++ src/autorunpy/util.py
@@ -1,30 +1,11 @@
-"""
-
-    """
-
 import json
-from dataclasses import dataclass
 from pathlib import Path
 
 class Conf :
-    url = 'url'  # GitHub url of the target repo
+    pkg = 'pip_pkg'  # GitHub url of the target repo
     py_ver = 'py_ver'  # python version to use
     module = "module"  # module name to run
-
-@dataclass
-class UserRepo :
-    user_name: str
-    repo_name: str
-    user_slash_repo: str
-    user_und_repo: str
-
-def get_user_repo_from_url(repo_url) :
-    gu = repo_url.split('github.com/')[1]
-    user_name = gu.split('/')[0]
-    repo_name = gu.split('/')[1]
-    user_slash_repo = f'{user_name}/{repo_name}'
-    user_und_repo = f'{user_name}_{repo_name}'
-    return UserRepo(user_name , repo_name , user_slash_repo , user_und_repo)
+    rm_venv = 'rm_venv'  # whether to remove venv
 
 def read_json(fp) :
     # if fp is not entered with .json extension, add it
