d8b7a1a6abdf5b1f52f5c8c44cce41ac1cd00597
imahdimir <imahdimir@gmail.com>
2024-05-06 21:58:28 -0700

    8.1.8

--- src/autorunpy/make_venv.py
+++ src/autorunpy/make_venv.py
@@ -19,11 +19,11 @@ def make_venv(conf_stem) :
     py_ver = j[c.py_ver]
 
     _cmds = ['pyenv' , 'install' , '--skip-existing' , py_ver]
-    _cmds += ['&>/dev/null']
+    _cmds += ['& >/dev/null']
     subprocess.run(_cmds)
 
     _cmds = ['pyenv' , 'virtualenv' , py_ver , conf_stem]
-    _cmds += ['&>/dev/null']
+    _cmds += ['& >/dev/null']
     subprocess.run(_cmds)
 
     print(conf_stem)
